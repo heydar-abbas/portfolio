812cd04da27569c862a8a86c37849f2b4c946aed
heydar-abbas <haydarabbas40@proton.me>
2025-02-16 17:49:04 +0300

    Fix hamburger and close button bug

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ const hamburger = document.querySelector(".hamburger");
 const close_menu = document.querySelector(".close-menu");
 const nav_list = document.querySelector(".nav-list");
 
-hamburger.addEventListener("click", function() {
-  nav_list.classList.add("open");
-  hamburger.classList.add("hide-hamburger");
-  close_menu.classList.remove("hide-close-menu");
-});
+hamburger.onclick = function() {
+  this.classList.add("hide-hamburger");
+  close_menu.classList.remove("hide-close-btn");
+  nav_list.classList.add("open-menu");
+};
 
-close_menu.addEventListener("click", function() {
-  nav_list.classList.remove("open");
-  close_menu.classList.add("hide-close-menu");
+close_menu.onclick = function() {
+  this.classList.add("hide-close-btn");
   hamburger.classList.remove("hide-hamburger");
-});
+  nav_list.classList.remove("open-menu");
+};
