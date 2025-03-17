65f683a86e869c7a6608e999e4daaac47e974573
heydar-abbas <haydarabbas40@proton.me>
2025-03-17 18:08:38 +0300

    Add 'pages' directory and 'robots.txt' file

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
       rollupOptions: {
         input: {
           main: resolve(__dirname, "index.html"),
-          works: resolve(__dirname, "works/index.html"),
+          works: resolve(__dirname, "pages/works/index.html"),
         },
       },
     },
